c2d6004b3198995d8c9184f79e3d2d6a367638ea
sufeng <sufeng@vuclip.com>
2018-03-30 15:23:51 -0700

    test

---
+++ drm/src/main/java/com/vuclip/monorepo/drm/DrmApplication.java
@@ -0,0 +1,12 @@
+package com.vuclip.monorepo.drm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrmApplication.class, args);
+	}
+}
---
+++ personalization/src/main/java/com/vuclip/monorepo/personalization/PersonalizationApplication.java
@@ -0,0 +1,12 @@
+package com.vuclip.monorepo.personalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizationApplication.class, args);
+	}
+}
